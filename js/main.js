c5bbb925828c717db8d8583534740dd7d3cd56ce
Feifei HANG <l.daniel.hung@gmail.com>
2015-12-18 17:32:55 +0800

    almost done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 'use strict';
 (function (window, document) {
+  // Change header height on the fly.
   var header = document.getElementById('header');
   header.style.height = window.innerHeight + 'px';
 
   window.onresize = function () {
     header.style.height = window.innerHeight + 'px';
   };
+
+  // Set target=_blank for each external anchor.
+  var anchors = document.getElementsByTagName('a');
+  anchors = Array.prototype.slice.call(anchors);
+  anchors.forEach(function (anchor) {
+    if (anchor.getAttribute('rel') === 'external') {
+      anchor.setAttribute('target', '_blank');
+    }
+  });
 })(window, document);
